a2319d86e27a5ccac43ec38b6203b7a254e84446
Alex Kagarov <alexkagarov@gmail.com>
2020-05-09 18:54:40 +0300

    Initial Commit

---
+++ Caturday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Caturday
+//
+//  Created by Mac on 5/9/20.
+//  Copyright © 2020 hialex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
